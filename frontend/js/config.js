b1c03a7dc6840a9fd29ce4357df9567132b5edd6
Carlos Felipe Siqueira Batista Soares <37373901+carl0sfelipe@users.noreply.github.com>
2025-12-19 18:47:55 -0300

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -5,7 +5,9 @@
 const CONFIG = {
   // 🔐 Pagamentos
   PIX_KEY: 'b853b083-72c3-444b-9224-7cb0d6e7a724',
-  QR_CODE_SIZE: 200
+  QR_CODE_SIZE: 200,
+  API_URL: 'http://104.131.181.151:4001'
+
 };
 
 // Exportar para uso global
